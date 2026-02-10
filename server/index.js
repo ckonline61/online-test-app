2cd3d2a93c846e08841c35bc97863c065ebb3382
ckonline61 <ckonline61@gmail.com>
2026-02-10 07:14:29 +0530

    Add auth route alias

--- server/index.js
+++ server/index.js
@@ -21,6 +21,7 @@ app.get('/', (req, res) => {
 
 // Routes
 app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/auth', require('./routes/authRoutes'));
 app.use('/api/tests', require('./routes/testRoutes'));
 app.use('/api/admin', require('./routes/adminRoutes'));
 
